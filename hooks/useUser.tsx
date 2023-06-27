b0226dc783e26ba318c6fae7d82a75f787e95fff
LucioFider <fidelislucio404@gmail.com>
2023-06-27 11:36:40 +1000

    Creating the dialog box for login

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -4,7 +4,7 @@ import {
   useSessionContext,
   useUser as useSupaUser,
 } from "@supabase/auth-helpers-react";
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useState, useContext } from "react";
 
 type UserContextType = {
   accessToken: string | null;
@@ -76,4 +76,12 @@ export const MyUserContextProvider = (props: Props) => {
     subscription,
   };
   return <UserContext.Provider value={value} {...props} />;
+};
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (context === undefined) {
+    throw new Error("useUser must be used within a MyUserContextProvider");
+  }
+  return context;
 };
---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import Modal from "@/components/Modal";
+
+const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <Modal />
+    </>
+  );
+};
+
+export default ModalProvider;
